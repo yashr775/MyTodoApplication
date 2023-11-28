586a88a7852093f5bf55fcc1be725fa14fac0543
yashr775 <rajy29629@gmail.com>
2023-11-28 18:02:04 +0530

    Components route implemented

--- Frontend/src/App.tsx
+++ Frontend/src/App.tsx
@@ -1,10 +1,17 @@
 import "./App.css";
 import Home from "./components/home";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Signup from "./components/signup";
 
 function App() {
   return (
     <div>
-      <Home></Home>
+      <Router>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/signup" element={<Signup />} />
+        </Routes>
+      </Router>
     </div>
   );
 }
