986c7df0893e32b86091f1a5efb64530581df1ab
yashr775 <rajy29629@gmail.com>
2023-11-30 19:32:09 +0530

    Sign inapi connected to frontend

--- Frontend/src/components/signin.tsx
+++ Frontend/src/components/signin.tsx
@@ -1,11 +1,56 @@
+import { ChangeEvent, FormEvent, useState } from "react";
 import Navbar from "./navbar";
 
-const signin = () => {
+interface FormData {
+  email: string;
+  password: string;
+}
+
+const Signin: React.FC = () => {
+  const [formData, setFormData] = useState<FormData>({
+    email: "",
+    password: "",
+  });
+
+  const handleChange = (event: ChangeEvent<HTMLInputElement>) => {
+    const { name, value } = event.target;
+
+    setFormData((prevFormData) => ({
+      ...prevFormData,
+      [name]: value,
+    }));
+  };
+
+  const handleSubmitClick = async (event: FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+
+    const { email, password } = formData;
+
+    const response = await fetch("http://localhost:5000/api/user/login", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({ email, password }),
+    });
+
+    const json = await response.json();
+
+    if (json.success === true) {
+      localStorage.setItem("auth-token", json.token);
+    } else {
+      console.log("Danger Invalid details");
+    }
+  };
+
   return (
     <div>
       <Navbar />
       <div className="bg-pink-200 flex justify-center items-center h-screen">
-        <form className="bg-white opacity-100 rounded-3xl p-2 w-1/5 border border-black ">
+        <form
+          className="bg-white opacity-100 rounded-3xl p-2 w-1/5 border border-black "
+          onSubmit={handleSubmitClick}
+        >
           <h1 className="flex justify-center mt-4 text-2xl t">Sign In</h1>
           <br />
           <label htmlFor="" className="text-xl ml-6">
@@ -17,6 +62,8 @@ const signin = () => {
             type="text"
             id="email"
             name="email"
+            value={formData.email}
+            onChange={handleChange}
             required
           ></input>{" "}
           <br />
@@ -26,9 +73,11 @@ const signin = () => {
           <br />
           <input
             className="border border-black w-80 h-9 ml-6"
-            type="text"
+            type="password"
             id="password"
             name="password"
+            value={formData.password}
+            onChange={handleChange}
             required
           />{" "}
           <br />
@@ -41,4 +90,4 @@ const signin = () => {
   );
 };
 
-export default signin;
+export default Signin;
