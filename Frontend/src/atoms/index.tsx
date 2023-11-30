37fc94b06a16119472e6ba76ad4a633999690181
yashr775 <rajy29629@gmail.com>
2023-11-30 16:38:09 +0530

    Sign in page connected with backend api

---
+++ Frontend/src/atoms/index.tsx
@@ -0,0 +1,10 @@
+// import {atom} from 'recoil';
+
+// interface createUserType {
+//     name:string;
+//     email:string;
+//     password:string;
+// }
+
+// export const createUser = atom<createUserType>()
+4;
--- Frontend/src/components/signup.tsx
+++ Frontend/src/components/signup.tsx
@@ -1,11 +1,64 @@
+import { ChangeEvent, FormEvent, useState } from "react";
 import Navbar from "./navbar";
 
-const signup = () => {
+interface FormData {
+  name: string;
+  email: string;
+  password: string;
+}
+
+const Signup: React.FC = () => {
+  const [formData, setFormData] = useState<FormData>({
+    name: "",
+    email: "",
+    password: "",
+  });
+
+  const handleChange = (event: ChangeEvent<HTMLInputElement>) => {
+    const { name, value } = event.target;
+    setFormData((prevFormData) => ({
+      ...prevFormData,
+      [name]: value,
+    }));
+  };
+
+  const handleSubmitClick = async (event: FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+
+    try {
+      console.log("Form Data:", formData);
+      const { name, email, password } = formData;
+      const response = await fetch(
+        "http://localhost:5000/api/user/createuser",
+        {
+          method: "POST",
+          headers: {
+            "Content-Type": "application/json",
+          },
+          body: JSON.stringify({ name, email, password }),
+        }
+      );
+
+      const json = await response.json();
+
+      if (json.success === true) {
+        localStorage.setItem("auth-token", json.token);
+      } else {
+        console.log("danger Invalid details");
+      }
+    } catch (error) {
+      console.error("An error occurred:", error);
+    }
+  };
+
   return (
     <div>
       <Navbar />
       <div className="bg-pink-200 flex justify-center items-center h-screen">
-        <form className="bg-white opacity-100 rounded-3xl p-2 w-1/5 border border-black ">
+        <form
+          className="bg-white opacity-100 rounded-3xl p-2 w-1/5 border border-black "
+          onClick={handleSubmitClick}
+        >
           <h1 className="flex justify-center pt-2 pb-2 text-2xl t">Register</h1>
           <label htmlFor="name" className="text-xl ml-6 ">
             Name:
@@ -16,6 +69,8 @@ const signup = () => {
             type="text"
             id="name"
             name="name"
+            value={formData.name}
+            onChange={handleChange}
             required
           />{" "}
           <br />
@@ -28,6 +83,8 @@ const signup = () => {
             type="text"
             id="email"
             name="email"
+            value={formData.email}
+            onChange={handleChange}
             required
           ></input>{" "}
           <br />
@@ -40,11 +97,13 @@ const signup = () => {
             type="text"
             id="password"
             name="password"
+            value={formData.password}
+            onChange={handleChange}
             required
           />{" "}
           <br />
-          <button className="flex justify-center text-lg bg-blue-800 mt-2 rounded-md pb-2 pt-2  w-80 h-9 ml-6 text-white hover:bg-blue-950  ">
-            Sign Up
+          <button className="flex justify-center text-lg bg-blue-800 mt-4 rounded-md pb-2 pt-1 mb-4  w-80 h-9 ml-6 text-white hover:bg-blue-950  ">
+            Submit
           </button>
         </form>
       </div>
@@ -52,4 +111,4 @@ const signup = () => {
   );
 };
 
-export default signup;
+export default Signup;
